0357e95d077cb92ae043906142eeb7c952c32906
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2020-12-03 15:57:42 +0100

    Change report dialog for report frame

--- ontimize-core-client/src/main/java/com/ontimize/report/DefaultReportDialog.java
+++ ontimize-core-client/src/main/java/com/ontimize/report/DefaultReportDialog.java
@@ -91,7 +91,6 @@ import com.ontimize.db.query.ParameterValuesDialog;
 import com.ontimize.gui.ApplicationManager;
 import com.ontimize.gui.Form;
 import com.ontimize.gui.MessageDialog;
-import com.ontimize.gui.container.EJDialog;
 import com.ontimize.gui.field.DataField;
 import com.ontimize.gui.field.ListDataField;
 import com.ontimize.gui.i18n.Internationalization;
@@ -654,6 +653,7 @@ public class DefaultReportDialog {
             ((Frame) this.container).setIconImage(ApplicationManager.getApplication().getFrame().getIconImage());
         }
         
+        
         this.bundle = res;
         if ((templateList == null) || (templateList.size() == 0)) {
             this.initTemplateList();
@@ -684,7 +684,7 @@ public class DefaultReportDialog {
                 // No reports enabled
             }
         }
-        this.container = new JFrame(DefaultReportDialog.TITLE_KEY);
+        this.container = new EJFrame(ApplicationManager.getTranslation(DefaultReportDialog.TITLE_KEY, this.bundle));
         if (this.container != null) {
             ((Frame) this.container).setIconImage(ApplicationManager.getApplication().getFrame().getIconImage());
         }
@@ -800,8 +800,10 @@ public class DefaultReportDialog {
                 // No reports enabled
             }
         }
-        this.container = new EJDialog(d, DefaultReportDialog.TITLE_KEY, true);
-        ((EJDialog) this.container).setAutoPackOnOpen(false);
+        this.container = new EJFrame(ApplicationManager.getTranslation(DefaultReportDialog.TITLE_KEY, this.bundle));
+        if (this.container != null) {
+            ((Frame) this.container).setIconImage(ApplicationManager.getApplication().getFrame().getIconImage());
+        }
         this.model = m;
         this.title = tit;
         this.bundle = res;
@@ -1891,8 +1893,6 @@ public class DefaultReportDialog {
     public void setDefaultCloseOperation(int operation) {
         if (this.container instanceof JFrame) {
             ((JFrame) this.container).setDefaultCloseOperation(operation);
-        } else if (this.container instanceof EJDialog) {
-            ((EJDialog) this.container).setDefaultCloseOperation(operation);
         }
     }
 
@@ -2045,8 +2045,6 @@ public class DefaultReportDialog {
     public void setJMenuBar(JMenuBar jMenuBar) {
         if (this.container instanceof JFrame) {
             ((JFrame) this.container).setJMenuBar(jMenuBar);
-        } else if (this.container instanceof EJDialog) {
-            ((EJDialog) this.container).setJMenuBar(jMenuBar);
         }
     }
 
@@ -2367,9 +2365,7 @@ public class DefaultReportDialog {
     }
 
     public void setAction(int keyCode, int modifiers, Action action, String key) {
-        if (this.container instanceof EJDialog) {
-            ((EJDialog) this.container).setAction(keyCode, modifiers, action, key);
-        } else if (this.container instanceof JFrame) {
+        if (this.container instanceof JFrame) {
 
             final int keyC = keyCode;
             final int modif = modifiers;
@@ -3068,9 +3064,6 @@ public class DefaultReportDialog {
 
     public void updateReport() {
         this.reportEngine.updateReport(false);
-        if ((this.container != null) && (this.container instanceof EJDialog)) {
-            ((EJDialog) this.container).setTitle(this.title);
-        }
     }
 
     public static Hashtable getParameters(Node node) {
@@ -3186,9 +3179,6 @@ public class DefaultReportDialog {
     public Container getContentPane() {
         if (this.container instanceof JFrame) {
             return ((JFrame) this.container).getContentPane();
-        }
-        if (this.container instanceof EJDialog) {
-            return ((EJDialog) this.container).getContentPane();
         }
         return null;
     }
